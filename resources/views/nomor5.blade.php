c7cc6a2c519715b9cbffb29832bb2a131130ed52
dhymaimha021@gmail.com <dhymaimha021@gmail.com>
2021-11-09 11:26:22 +0700

    soal nomor 6

--- resources/views/nomor5.blade.php
+++ resources/views/nomor5.blade.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <p>muqooo</p>
+    <p>Version Control System Merupakan Suatu paket software dimana ketika kita meninisialisasi, VCS akan memonitor file kita untuk setiap perubahan dan mengizinkan kita untuk menandai perubahan tersebut pada setiap level perubahan , Sehingga kita dapat meninjau ulang tahapan yang ditandai kapan pun ketika dibutuhkan .</p>
+    Mercurial
+Monotone
+Bazaar
+TFS
+VSTS
+Perforce Helix Core
+IBM Rational ClearCase
 </body>
 </html>
